075dc839640da4cc552515a1e3547ff1e21cb721
sammun1926 <munorusam10@gmail.com>
2023-03-18 07:58:20 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const divide = ()  => (200 / 10);
+
+divide();
+
+const square = (param1) => param1 ** 2;
+const squareAns = square(2);
+
+let a = 3;
+let b = 4;
+const add = (param1, param2) => param1 + param2;
+const addAns = add(a, b);
